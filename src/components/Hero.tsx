3ad4365b9a34d7025d61b056f2f5ba4c82df165a
Abasiafak Udosen, PhD <137122196+Abasfreky1@users.noreply.github.com>
2025-12-27 03:00:50 -0500

    Change header size for Hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -27,7 +27,7 @@ export function Hero() {
                 Research Assistant @ RoCkeTEd Lab | Purdue Polytechnic
               </span>
             </div>
-            <h1 className="text-5xl md:text-6xl font-bold text-gray-900 leading-tight">
+            <h1 className="text-4xl md:text-5xl font-bold text-gray-900 leading-tight">
               Abasiafak Udosen PhD(ABD)
             </h1>
 
